c666dfe21da929f4ada953adb27c044ab3ed9f91
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-11-19 17:44:49 +0100

    created SignIn handler

--- api/go.mod
+++ api/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.2.0
 	github.com/gofiber/jwt/v2 v2.1.0
 	github.com/klauspost/compress v1.11.3 // indirect
-	github.com/prisma/prisma-client-go v0.1.0 // indirect
+	github.com/prisma/prisma-client-go v0.1.0
+	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 )
--- api/router/auth.go
+++ api/router/auth.go
@@ -1,11 +1,12 @@
 package router
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/FranciscoMendes10866/listry/api/auth"
+	"github.com/gofiber/fiber/v2"
+)
 
 // AuthRouter ...
 func AuthRouter(app *fiber.App) {
 	api := app.Group("/api/v1/auth")
-	api.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Auth Router")
-	})
+	api.Post("/sign-up", auth.SignUp)
 }
