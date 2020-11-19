128db2fbdedd4611dcb0cb85863a37bd86be7547
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-11-19 19:08:43 +0100

    added cors, helmet, compress, logger

--- api/go.mod
+++ api/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/form3tech-oss/jwt-go v3.2.2+incompatible
 	github.com/gofiber/fiber v1.14.6
 	github.com/gofiber/fiber/v2 v2.2.0
+	github.com/gofiber/helmet/v2 v2.0.1
 	github.com/gofiber/jwt v0.2.0
 	github.com/gofiber/jwt/v2 v2.1.0
 	github.com/klauspost/compress v1.11.3 // indirect
--- api/main.go
+++ api/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"github.com/FranciscoMendes10866/listry/api/router"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/helmet/v2"
 )
 
 func main() {
 	app := fiber.New()
-
+	app.Use(cors.New())
+	app.Use(helmet.New())
+	app.Use(logger.New())
+	app.Use(compress.New())
 	router.AuthRouter(app)
 	router.AdressesRouter(app)
 
